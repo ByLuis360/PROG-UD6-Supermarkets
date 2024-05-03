d79a96e0f46af2e3ede4e00e87d7d7dd65a4346d
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-05-04 00:43:07 +0100

    feat: clase cajero completada

--- src/es/supermercados/mercadona/Cajero.java
+++ src/es/supermercados/mercadona/Cajero.java
@@ -6,10 +6,12 @@ import java.util.Queue;
 public class Cajero {
     private String numeroCajero;
     private Queue<Cliente> filaClientes;
+    private boolean cajaAbierta;
 
-    public Cajero(String numeroCajero, Queue<Cliente> filaClientes) {
+    public Cajero(String numeroCajero, Queue<Cliente> filaClientes, boolean cajaAbierta) {
         this.numeroCajero = numeroCajero;
         this.filaClientes = new LinkedList<>();
+        this.cajaAbierta = false;
     }
 
     public String getNumeroCajero() {
@@ -28,11 +30,50 @@ public class Cajero {
         this.filaClientes = filaClientes;
     }
 
-    public Queue<Cliente> añadirClienteAleatorio() {
-        Cliente clienteAñadido = new Cliente(null, null);
-        this.filaClientes.add(clienteAñadido);
-        return this.filaClientes;
+    public boolean isCajaAbierta() {
+        return cajaAbierta;
+    }
 
+    public void setCajaAbierta(boolean cajaAbierta) {
+        this.cajaAbierta = cajaAbierta;
+    }
+
+    public void abrirCaja() {
+        if (cajaAbierta) {
+            System.out.println("La caja ya estaba abierta antes");
+        } else {
+            cajaAbierta = true;
+            System.out.println("Se ha abierto la caja");
+        }
+    }
+
+    public void añadirClienteAleatorio() {
+        if (cajaAbierta) {
+            Cliente clienteAñadido = new Cliente(null, null);
+            this.filaClientes.add(clienteAñadido);
+            System.out.println(clienteAñadido);
+        } else {
+            System.out.println("No se puede añadir un cliente a la cola porque la caja está cerrada");
+        }
+    }
+
+    public void atenderCliente() {
+        if (!filaClientes.isEmpty()) {
+            Cliente clienteAtendido = filaClientes.poll();
+            System.out.println("El cliente " + clienteAtendido.getNombre() + " ha sido atendido");
+        } else {
+            System.out.println("No hay ningún cliente en la fila");
+        }
+    }
+
+    public void cerrarSupermercado() {
+        if (filaClientes.isEmpty()) {
+            cajaAbierta = false;
+            System.out.println("La caja está cerrada y el mercadona también cierra por hoy hasta pornto!");
+        } else {
+            System.out.println(
+                    "No se puede cerrar la caja hasta que no termines de atender a todos los clientes, anda espabila que la vida te va a coméh");
+        }
     }
 
     @Override
@@ -41,6 +82,9 @@ public class Cajero {
                 + "\n* Total de clientes: " + this.filaClientes.size() + "\n* Clientes en la fila:\n";
         for (Cliente listaDeClientes : filaClientes) {
             mensaje += "\t" + listaDeClientes.getNombre() + "\n";
+        }
+        if (filaClientes.isEmpty()) {
+            System.out.println("No quedan clientes pendientes");
         }
         return mensaje;
     }
