146c5e0baf8b05f4688f2af25331c66a7fd51b31
ByLuis360 <usr2477@salesianos-lacuesta.net>
2024-05-04 13:55:51 +0100

    feat: clase ejecutable en funcionamiento

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
+import es.supermercados.mercadona.Cajero;
+import es.supermercados.mercadona.menu.Menu;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final Menu MENU = new Menu();
+        Cajero caja = new Cajero("1", null, false);
+        boolean seguirPreguntando = true;
+        String opcion;
+        while (seguirPreguntando) {
+            System.out.println(MENU.mostrarMenu());
+            opcion = scanner.nextLine();
+            switch (opcion) {
+                case "1":
+                    caja.abrirCaja();
+                    break;
+                case "2":
+                    caja.añadirClienteAleatorio();
+                    break;
+                case "3":
+                    caja.atenderCliente();
+                    break;
+                case "4":
+                    System.out.println(caja);
+                    break;
+                case "5":
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
 
     }
 }
